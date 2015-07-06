053bf9ced52027d7a07c93ac7252ba724b17184d
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-07-06 16:39:25 +0200

    remove body, or have a budy if we have a "?debug" query string, because swift also does not have a body while putting something.

--- test/run_test_app.py
+++ test/run_test_app.py
@@ -2,14 +2,13 @@ def sample_app(environ, start_response):
     body = []
     keys = ['PATH_INFO', 'QUERY_STRING', 'REQUEST_METHOD',
             'HTTP_HEADERNAME', 'wsgi.input']
-    for key in keys:
-        value = environ.get(key)
-        body.append('{}: \'{}\'\n'.format(key, value))
-    if environ.get('HTTP_X_OBJECT_META_PID'):
-        body.append('HTTP_X_OBJECT_META_PID: \'{}\'\n'.
-                    format(environ.get('HTTP_X_OBJECT_META_PID')))
-
-    headers = [('Content-Type', 'text/plain')]
+    if 'debug' in environ['QUERY_STRING']:
+        for key in keys:
+            value = environ.get(key)
+            body.append('{}: \'{}\'\n'.format(key, value))
+        headers = [('Content-Type', 'text/plain')]
+    else:
+        headers = []
     start_response('201 Created', headers)
     return body
 
